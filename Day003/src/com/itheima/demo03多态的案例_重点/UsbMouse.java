9a7005c15b95f977b1daf65af5cdd2149ad75a2f
Leonidas <leonidas.geng@outlook.com>
2022-11-14 10:58:39 -0500

    Update UsbMouse.java

--- Day003/src/com/itheima/demo03多态的案例_重点/UsbMouse.java
+++ Day003/src/com/itheima/demo03多态的案例_重点/UsbMouse.java
@@ -1,18 +1,20 @@
 package com.itheima.demo03多态的案例_重点;
+/*USB鼠标实现USB接口功能
 
-public class UsbMouse implements Usb {
+ */
+public class USBMouse implements USB{
+    //常规功能1，插入/链接
     @Override
     public void connect() {
-        System.out.println("Usb鼠标连接啦!");
+        System.out.println("USB鼠标连接成功");
     }
-
+    //常规功能2，拔出/断开
     @Override
-    public void disConnect() {
-        System.out.println("Usb鼠标断开啦!");
+    public void disconnect() {
+        System.out.println("USB鼠标已断开");
     }
-
-    // 特有方法(点击)
-    public void click() {
-        System.out.println("Usb鼠标点击啦!!!!!!!!!!!!!");
+    //特有功能
+    public static void click(){
+        System.out.println("USB鼠标点了一下");
     }
 }
