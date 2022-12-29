1f45685a96d7813be7129c568ba79e7ea9215e61
Leonidas <leonidas.geng@outlook.com>
2022-12-28 19:47:08 -0500

    202212281948

---
+++ Day010/src/_01/File_Constructor/demo01.java
@@ -0,0 +1,44 @@
+package _01.File_Constructor;
+
+import java.io.File;
+
+/*
+File class
+作用
+    java.io.File代表操作系统的文件/文件夹对象
+    通过File对象操作文件和文件夹
+
+新建
+删除
+看文件/文件夹信息（属性）
+
+File类有好多构造器
+最主要的有3个
+
+    1, File(String pathname)  File(D:\2022年8月线下完结JAVAEE课程\02阶段JAVA进阶(每天看3个)\day10-File、递归、IO流\2-讲义)
+
+在window里，//和\\都可以
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        //1,File(String pathname)  File(D:\2022年8月线下完结JAVAEE课程\02阶段JAVA进阶(每天看3个)\day10-File、递归、IO流\2-讲义)
+        //字符串里黏贴，自动打上双\\，如果没有自己要添加
+        File file1 = new File("C:\\Software\\MyFileTest");
+        //File file1 = new File("C://Software//MyFileTest");
+        System.out.println("File1 = "+file1);
+
+        //2, File(String parent, String child) 通过父路径和子路径创建File对象
+        File file2 = new File("C:\\Software","MyFileTest");
+        //File file2 = new File("C://Software","MyFileTest");
+        System.out.println("File2 = "+file2);
+
+        //3,File(File parent, File child) 通过父路径File对象和子路径创建File对象
+        File parent = new File("C:\\Software");
+        File file3 = new File(parent, "MyFileTest");
+        //File parent = new File("C://Software");
+        //File file3 = new File(parent, "MyFileTest");
+        System.out.println("file3 = "+file3);
+
+
+    }
+}
---
+++ Day010/src/_02/File_Delete/demo02.java
@@ -0,0 +1,25 @@
+package _02.File_Delete;
+
+import java.io.File;
+
+/*
+public boolean delete() 删除文件或文件夹
+
+直接删除，不走回收站，不可撤回
+如果是一个文件，直接删除
+如果是一个文件夹，需要先删除文件夹中的内容，最后才能删除文件夹
+ */
+public class demo02 {
+    public static void main(String[] args) {
+        //定位文件
+        //1，boolean delete() 可以删除文件，删除成功返回true
+        File file1 = new File("C:\\Software\\MyFileTest\\test1\\test1.txt");
+        System.out.println(file1.delete());
+
+        //2，boolean delete() 删除空文件夹
+        File file2 = new File("C:\\Software\\MyFileTest\\test3"); //空文件夹，true
+        System.out.println("file2.delete()");
+        File file3 = new File("C:\\Software\\MyFileTest\\test2"); //里面有文件，false
+        System.out.println("file3.delete()");
+    }
+}
